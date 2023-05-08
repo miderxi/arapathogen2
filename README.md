98fe3488e0f14640cce513a1d4dfa6beb48cb151
miderxi <1965770446@qq.com>
2023-05-08 22:12:54 +0800

    readme

---
+++ README.md
@@ -0,0 +1,10 @@
+#1. generate feature of EsmMean and AraNetSturc2vec
+python ./features/EsmMean/extract2.py
+python ./features/AraNetStruc2vec/gen_struc2vec_v3.py
+
+#2. run train and test
+bash ./script/run.sh
+
+
+#3. visit our web server
+http://zzdlab.com/intersppi/arapathogen/index.php
--- script/run.sh
+++ script/run.sh
@@ -1,12 +1,3 @@
-#comparation of sequence-base methods.
-python 10folds_RF_C1223.py dpc
-python 10folds_RF_C1223.py ct
-python 10folds_RF_C1223.py ac
-python 10folds_RF_C1223.py cksaap
-python 10folds_RF_C1223.py EsmMean
-python 10folds_RF_C1223.py doc2vec
-python 10folds_RF_C1223.py prottrans
-
 #comparation of graph node embedding
 python 10folds_RF_C1223.py AraNetStruc2vec
 python 10folds_RF_C1223.py AraNetNode2vec
@@ -17,7 +8,6 @@ python 10folds_NN_C1223.py  EsmMean AraNetStruc2vec
 python 10folds_SVM_C1223.py EsmMean AraNetStruc2vec
 python 10folds_Xgboost.py   EsmMean AraNetStruc2vec
 
-
 #comparation of sequence-base methods
 python 10folds_Xgboost_C1223.py dpc
 python 10folds_Xgboost_C1223.py ct
@@ -26,7 +16,6 @@ python 10folds_Xgboost_C1223.py cksaap
 python 10folds_Xgboost_C1223.py EsmMean
 python 10folds_Xgboost_C1223.py doc2vec
 python 10folds_Xgboost_C1223.py prottrans
-
 
 #comparation of graph embedding
 python 10folds_Xgboost_C1223.py AraNetStruc2vec
