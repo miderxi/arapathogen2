3e1b3eec8b9a644a8b1694e970c148b232e313b3
miderxi <1965770446@qq.com>
2023-05-15 21:34:44 +0800

    adjusting file names

--- script/RF_features.py
+++ script/RF_features.py
@@ -33,7 +33,6 @@ class Features_C123:
         feature_EsmMean   = MyFeature(["../features/EsmMean/ara_and_eff_only_need_esm2.pkl"])  #shape(2560,)
         feature_prottrans = MyFeature(["../features/prottrans/ara_and_eff_prottrans_v2.pkl"]) #shape (1024,)
         feature_prottrans = MyFeature(["../features/prottrans/ara_and_eff_only_need_prottrans.pkl"]) #shape (1024,)
-        feature_doc2vec   = MyFeature(["../features/doc2vec/ara_and_eff_doc2vec_128.pkl"])  #shape (400,)
         
         feature_ac  = MyFeature(["../features/AC/ara_and_eff_AC_210.pkl"])  #shape(210,)
         feature_ct  = MyFeature(["../features/CT/ara_and_eff_CT_343.pkl"])  #shape (343,)
@@ -43,18 +42,11 @@ class Features_C123:
         feature_AraNetStruc2vec = MyFeature(["../features/AraNetStruc2vec/AraNetStruc2vec_256_v3.pkl"],allow_empty=True)
         feature_AraNetNode2vec  = MyFeature(["../features/AraNetNode2vec/AraNet_node2vec_256.pkl"],allow_empty=True) #
         
-        #feature_EsmMsa    = MyFeature(["../features/esm-msa/ara_and_eff_esm-msa.pkl"])  #shape (768,)
-        #feature_sublocation = MyFeature(["../features/sublocation/sublocaiton_34.pkl"],allow_empty=True)       
-        #feature_DMINode2vec = MyFeature(['../features/dmi_node2vec/ara_and_eff_dmi_node2vec_128.pkl'],allow_empty=True)
-        #feature_PSP    = MyFeature(["../features/SecondaryStructure/features_secd.pkl"])
-        feature_AraNetProperty = MyFeature(["../features/AraNetProperty/AraNetProperty_7_v1.pkl"],allow_empty=True)
-        #feature_AraGeo = MyFeature(["../features/gene_expression/geo_111.pkl"],allow_empty=True)
 
         self.info = info
         self.features={
             'EsmMean':feature_EsmMean,
             'prottrans':feature_prottrans,
-            'doc2vec':feature_doc2vec,
             
             'ac':feature_ac,
             'ct':feature_ct,
@@ -63,13 +55,6 @@ class Features_C123:
             
             'AraNetNode2vec':feature_AraNetNode2vec,
             'AraNetStruc2vec':feature_AraNetStruc2vec,
-            #'EsmMsa':feature_EsmMsa,
-            #'PSP':feature_PSP,
-            #'DMINode2vec':feature_DMINode2vec,
-            #'stringNet':feature_StringAraNetStruc2vec
-            'AraNetProperty':feature_AraNetProperty,
-            #'AraGeo':feature_AraGeo,
-            #'sublocation':feature_sublocation,
             }
     
     def get(self,index,foldn=None):
